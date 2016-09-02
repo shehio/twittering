018895130a6f7c96e429fb4175157cbbb68e15ed
shehio <shehabyasser@gmail.com>
2016-09-02 18:43:04 -0400

    starting point for testing our use in geo module.

--- test/index.js
+++ test/index.js
@@ -4,15 +4,26 @@ const server = require('./../server');
 const geo = require('./../geo');
 const twitter = require('./../twitter');
 const assert = require('assert');
-var request = require('supertest');
+const expect = require('chai').expect;
+const request = require('supertest');
+
 var app = null;
+
+/**
+ *  Writing endless tests can be time-consuming, and counterproductive sometimes.
+ *  We write tests that make sense.
+ */
+
 
 /**
  *  server suite
  */
 describe('server: ', () => {
     before(() => { console.log('starting the server, activating paths before the tests.'); server.start(); app = server.activate(); });
-    after(() => { console.log('killing the server after the tests. '); server.stop(); });
+    /**
+     * @todo: figure a way to kill the server other than the trivial process.exit()
+     */
+    // after(() => { console.log('killing the server after the tests. '); server.stop(); });
     /**
      *  @todo: add more context
      *  -- test the body.
@@ -38,7 +49,29 @@ describe('geo: ', () => {
  *  twitter suite
  */
 describe('geo: ', () => {
+    const city = 'san francisco';
+    const latitude = 37.7749295;
+    const longitude = -122.4194155;
+    /**
+     * subtly, it's testing the module, but in a sense, it still testing the function.
+     */
+    it('tests cityToCoords function', (done) => {
+        geo.cityToCoords(city).
+            then((response) => {
+                expect(response).to.be.a('Array');
+                done();
+            });
+    });
 
+    it('tests cityToCoords function', (done) => {
+        geo.cityToCoords(city).
+            then((retrieved) => geo.extractCoords(retrieved)).
+            then((retrieved) => {
+                assert.equal(retrieved.latitude, latitude);
+                assert.equal(retrieved.longitude, longitude);
+                done();
+            });
+    });
 
 });
 
@@ -53,7 +86,7 @@ describe('index: ', () => {
 /**
  *  middleman suite
  */
-describe('geo: ', () => {
+describe('middleman: ', () => {
 
 
 });
